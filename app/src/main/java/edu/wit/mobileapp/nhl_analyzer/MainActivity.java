5997067a8426289705a482c4be2a5ce3bd23552d
jcruz9678 <jcruz9678@gmail.com>
2018-06-29 17:27:31 -0400

    test

---
+++ app/src/main/java/edu/wit/mobileapp/nhl_analyzer/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.wit.mobileapp.nhl_analyzer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//test
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
