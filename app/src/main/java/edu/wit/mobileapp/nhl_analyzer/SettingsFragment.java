93f425b72166dfa1db68caa6e40669976f5dc0bf
jcruz9678 <jcruz9678@gmail.com>
2018-08-02 08:24:46 -0400

    Button to change photo in Settings tab

--- app/src/main/java/edu/wit/mobileapp/nhl_analyzer/SettingsFragment.java
+++ app/src/main/java/edu/wit/mobileapp/nhl_analyzer/SettingsFragment.java
@@ -1,24 +1,31 @@
 package edu.wit.mobileapp.nhl_analyzer;
 
-import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
-import android.media.Image;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatDelegate;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Switch;
 
-import javax.xml.transform.Result;
+import java.io.IOException;
+
+import static android.app.Activity.RESULT_OK;
 
 public class SettingsFragment extends Fragment
 {
     private static int PICK_IMAGE = 1;
+    private static Bitmap bitmap;
+    ImageView imageview;
 
     public SettingsFragment()
     {
@@ -34,7 +41,6 @@ public class SettingsFragment extends Fragment
 
         Button browseButton = (Button) view.findViewById(R.id.browse_button);
 
-        //TODO finish button to allow users to change home stock image
         browseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -68,16 +74,30 @@ public class SettingsFragment extends Fragment
 
 
     public void openImageBrowser(View view){
-        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        Intent galleryIntent = new Intent();
         galleryIntent.setType("image/*");
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(Intent.createChooser(galleryIntent, "Select Picture"), PICK_IMAGE);
+        Log.d("TAG", "Browser opened");
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data){
-        Image userImg;
-        if(requestCode == PICK_IMAGE){
+        super.onActivityResult(requestCode, resultCode, data);
 
+        View v = getView();
+
+        if(requestCode == PICK_IMAGE ){
+            Uri uri = data.getData();
+
+            try{
+                bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), uri);
+                Log.d("TAG", String.valueOf(bitmap));
+
+
+            } catch (IOException e){
+                e.printStackTrace();
+            }
         }
     }
 
